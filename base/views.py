17349bce1d113bba64099e553eb6b72871df7a51
chrisjire <christianwilliamsjire@gmail.com>
2022-09-22 15:47:49 +0300

    first commit

---
+++ base/views.py
@@ -0,0 +1,28 @@
+from multiprocessing import context
+from django.shortcuts import render
+from .models import Room
+
+
+# Create your views here.
+
+
+#rooms = [
+ #   {'id':1, 'name':'lets learn python!'},
+  #  {'id':2, 'name':'lets learn java!'},
+   # {'id':3, 'name':'design with me'},
+    #{'id':4, 'name':'UX design'},
+#]
+
+def home(request):
+    rooms = Room.objects.all()
+    context = {'rooms':rooms}
+    return render(request, 'base/home.html', context)
+
+def room(request, pk):
+    room = Room.objects.get(id=pk)
+    context = {'room':room}
+    return render(request, 'base/room.html', context)
+
+def createRoom(request):
+    context = {}
+    return render(request, 'base/room_form.html', context)
